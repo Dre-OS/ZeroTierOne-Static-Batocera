072f9fe8c8303b855811e1578d89c3b0e0d81c8b
Trent <hlsagabaen@gmail.com>
2024-04-24 11:47:47 +0800

    changed lib directory and added additional support for multiple sys architectures

--- Batocera.sh
+++ Batocera.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-LINK_OR_DIR="/usr/lib/zerotier-one"
+LINK_OR_DIR="/var/lib/zerotier-one"
 DEST_DIR="/userdata/system/configs/zerotier"
 SYS_ARCH=$(arch)
 
 # Download zerotier
-if [ $SYS_ARCH == "aarch64" ]; then
+if [ $SYS_ARCH = "aarch64" ]; then
     curl -LJO https://github.com/Dre-OS/ZeroTierOne-Static-Batocera/releases/latest/download/zerotier-one-aarch64.tar.gz
-elif [ $SYS_ARCH == "x86_64" ]; then
+elif [ $SYS_ARCH = "x86_64" ]; then
     curl -LJO https://github.com/Dre-OS/ZeroTierOne-Static-Batocera/releases/latest/download/zerotier-one-x86_64.tar.gz
 else
     echo ERROR: Unsupported system architecture
@@ -16,14 +16,25 @@ fi
 
 # Unpack downloaded archive
 mkdir ~/bin
-tar -xzf zerotier-one-aarch64.tar.gz bin/zerotier-one -C ./bin
+if [ $SYS_ARCH = "aarch64" ]; then
+    tar -xzf zerotier-one-aarch64.tar.gz bin/zerotier-one -C ./bin
+elif [ $SYS_ARCH = "x86_64" ]; then
+    tar -xzf zerotier-one-x86_64.tar.gz bin/zerotier-one -C ./bin
+else
+    echo ERROR: Unsupported system architecture
+    exit 1 # terminate and indicate error
+fi
 
 # Install to root bin directory
 install bin/* /usr/bin
 
 # If you want, you can remove the archive
-rm zerotier-one-aarch64.tar.gz
-
+if [ $SYS_ARCH = "aarch64" ]; then
+    rm zerotier-one-aarch64.tar.gz
+elif [ $SYS_ARCH = "x86_64" ]; then
+    rm zerotier-one-x86_64.tar.gz
+fi
+ 
 # add links to cli and idtool
 cd "/usr/bin" || exit
 ln -s zerotier-one zerotier-cli
@@ -37,11 +48,8 @@ zerotier-one
 pkill zerotier-one
 
 # Add configs to /userdata/system
-mv "$LINK_OR_DIR" "$DEST_DIR"
+mv "$LINK_OR_DIR/"* "$DEST_DIR"
 ln -s "$DEST_DIR" "$LINK_OR_DIR"
 
 # Start Zerotier
 zerotier-one
-
-# logging
-# bash 2>&1 | tee ~/batocera.log
